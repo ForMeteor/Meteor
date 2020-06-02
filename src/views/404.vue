8ea13ec68dd1f5cafe6adbc712f6e01bb57e951e
CuiLi <525381500@qq.com>
2020-06-02 16:33:31 +0800

    62

---
+++ src/views/404.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+  <div>
+      <div>404 not found</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '404',
+  data () {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+
+</script>
+<style  scoped>
+</style>
