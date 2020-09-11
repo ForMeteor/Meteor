b0ac872495f1586bff6ab697ad241875389b10a6
CuiLi <525381500@qq.com>
2020-09-11 15:27:53 +0800

    itest

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,7 +1,7 @@
 const getters = {
-    appName: state => state.app.appName,
-    catShow: state => state.permission.catShow,
-    backShow: state => state.permission.backShow
+    appName: (state:any) => state.app.appName,
+    catShow: (state:any) => state.permission.catShow,
+    backShow: (state:any) => state.permission.backShow
   }
   export default getters
   
