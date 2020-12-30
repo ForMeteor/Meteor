9ac288f95508c2d74750f593e31d061b9323124b
ForMeteor <525381500@qq.com>
2020-12-30 23:54:53 +0800

    upload

---
+++ src/views/practice/sqlDeal.vue
@@ -0,0 +1,33 @@
+<!-- 处理数据库 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+import { adminTable } from '@/api/admin'
+export default {
+  name : '',
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {
+  },
+
+  methods: {
+    getList(){
+      adminTable().then(()=>{
+        console.log("done")
+      }).catch()
+    }
+  }
+}
+
+</script>
+<style  scoped>
+</style>
